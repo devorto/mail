8464b62126e6bcfc0f0e7ce798e4006e8ef363f4
Geoffrey Dijkstra <geoffrey.dijkstra@gmail.com>
2020-03-31 11:21:09 +0200

    Removed getters from interface.
    Removed getters from interface because they are never being used, we just set the data and send the mail.

--- src/Mail.php
+++ src/Mail.php
@@ -10,21 +10,11 @@ namespace Devorto\Mail;
 interface Mail
 {
     /**
-     * @return Recipient[]
-     */
-    public function getTo(): array;
-
-    /**
      * @param Recipient $recipient
      *
      * @return Mail
      */
     public function addTo(Recipient $recipient): Mail;
-
-    /**
-     * @return Recipient[]
-     */
-    public function getCc(): array;
 
     /**
      * @param Recipient $recipient
@@ -34,21 +24,11 @@ interface Mail
     public function addCc(Recipient $recipient): Mail;
 
     /**
-     * @return Recipient[]
-     */
-    public function getBcc(): array;
-
-    /**
      * @param Recipient $recipient
      *
      * @return Mail
      */
     public function addBcc(Recipient $recipient): Mail;
-
-    /**
-     * @return Recipient
-     */
-    public function getFrom(): Recipient;
 
     /**
      * @param Recipient $recipient
@@ -58,21 +38,11 @@ interface Mail
     public function setFrom(Recipient $recipient): Mail;
 
     /**
-     * @return Recipient
-     */
-    public function getReplyTo(): Recipient;
-
-    /**
      * @param Recipient $recipient
      *
      * @return Mail
      */
     public function setReplyTo(Recipient $recipient): Mail;
-
-    /**
-     * @return string
-     */
-    public function getSubject(): string;
 
     /**
      * @param string $subject
@@ -82,21 +52,11 @@ interface Mail
     public function setSubject(string $subject): Mail;
 
     /**
-     * @return Attachment[]
-     */
-    public function getAttachments(): array;
-
-    /**
      * @param Attachment $attachment
      *
      * @return Mail
      */
     public function addAttachment(Attachment $attachment): Mail;
-
-    /**
-     * @return string
-     */
-    public function getMessage(): string;
 
     /**
      * @param string $message
